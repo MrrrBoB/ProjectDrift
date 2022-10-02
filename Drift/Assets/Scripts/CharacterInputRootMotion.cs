3a2f8ed080db5eb67c2fbfea6517357be61ba496
NateMobile <nathoward22@gmail.com>
2022-10-01 19:10:26 -0600

    Wasted some time on jump and imported jump animations

--- Drift/Assets/Scripts/CharacterInputRootMotion.cs
+++ Drift/Assets/Scripts/CharacterInputRootMotion.cs
@@ -11,8 +11,7 @@ public class CharacterInputRootMotion : MonoBehaviour
     private Rigidbody rBody;
     public float speedMultiplier;
     private Vector2 move;
-
-    private float distToGround;
+    private bool canJump;
     //Legacy movement vectors, now are controlled via animation
     private Vector3 m;
     private Vector3 v;
@@ -30,16 +29,16 @@ public class CharacterInputRootMotion : MonoBehaviour
         ctrls.Player.Jump.performed += ctx => CharacterJump();
         ctrls.Player.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
         ctrls.Player.Move.canceled += ctx => move = Vector2.zero;
-        m = new Vector3(0f, 0f, Time.deltaTime * 2f);
-        j = new Vector3(0, 10, 10f);
-        r = new Vector3(0f, Time.deltaTime*20f, 0f);
+        
     }
 
     private void Start()
     {
-        distToGround = GetComponent<Collider>().bounds.extents.y;
+        m = new Vector3(0f, 0f, Time.deltaTime * 2f);
+        j = new Vector3(0, 10f, 10f);
+        r = new Vector3(0f, Time.deltaTime*20f, 0f);
+        SetJump(IsGrounded());
     }
-
     private void Update()
     {
         HandleMovement();
@@ -80,7 +79,6 @@ public class CharacterInputRootMotion : MonoBehaviour
         }
         else 
             cAnimator.SetFloat("TurnDirectionFloat", 0);
-        
     }
 
     public void CharacterJump()
@@ -88,16 +86,21 @@ public class CharacterInputRootMotion : MonoBehaviour
         if (IsGrounded())
         {
             rBody.AddForce(j, ForceMode.Impulse);
+            cAnimator.SetTrigger("Jump");
             Debug.Log("Boing");
         }
         else Debug.Log("Not Grounded");
     }
 
+    private void SetJump(bool val)
+    {
+        canJump = val;
+        cAnimator.SetBool("OnGround", val);
+    }
     private void OnEnable()
     {
         ctrls.Player.Enable();
     }
-
     private void OnDisable()
     {
         ctrls.Player.Disable();
