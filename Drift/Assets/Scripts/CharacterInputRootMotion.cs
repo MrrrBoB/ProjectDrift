5dd9494d68309785dcec2b93b510c3b57cb54f3d
Nate <Nathoward22@gmail.com>
2022-10-02 02:44:07 -0600

    Implemented full jump, but its kinda buggy

--- Drift/Assets/Scripts/CharacterInputRootMotion.cs
+++ Drift/Assets/Scripts/CharacterInputRootMotion.cs
@@ -14,12 +14,14 @@ public class CharacterInputRootMotion : MonoBehaviour
     private bool canJump;
     //Legacy movement vectors, now are controlled via animation
     private Vector3 m;
-    private Vector3 v;
+    private Vector3 rayOffset;
     private Vector3 r;
     private Vector3 j;
     private Animator cAnimator;
     private static readonly int IsRunning = Animator.StringToHash("IsRunning");
     private static readonly int RunDirectionFloat = Animator.StringToHash("RunDirectionFloat");
+    private static readonly int Jump = Animator.StringToHash("Jump");
+    private static readonly int OnGround = Animator.StringToHash("OnGround");
 
     void Awake()
     {
@@ -35,14 +37,16 @@ public class CharacterInputRootMotion : MonoBehaviour
     private void Start()
     {
         m = new Vector3(0f, 0f, Time.deltaTime * 2f);
-        j = new Vector3(0, 10f, 10f);
+        j = new Vector3(0, 10f, 1f);
         r = new Vector3(0f, Time.deltaTime*20f, 0f);
-        SetJump(IsGrounded());
+        rayOffset = new Vector3(0f, 0.5f, 0f);
+        cAnimator.SetBool("OnGround",IsGrounded());
     }
     private void Update()
     {
         HandleMovement();
         HandleRotation();
+        //Debug.DrawRay(transform.position, Vector3.down, Color.green);
     }
 
     public void MoveCharacter()
@@ -75,7 +79,7 @@ public class CharacterInputRootMotion : MonoBehaviour
         if (Mathf.Abs(move.x) >=0.5f)
         {
             cAnimator.SetFloat("TurnDirectionFloat", move.x);
-            transform.Rotate(r*move.x*(1+speedMultiplier), Space.World);
+            transform.Rotate(r * (move.x * (1+(speedMultiplier/2))), Space.World);
         }
         else 
             cAnimator.SetFloat("TurnDirectionFloat", 0);
@@ -85,18 +89,44 @@ public class CharacterInputRootMotion : MonoBehaviour
     {
         if (IsGrounded())
         {
-            rBody.AddForce(j, ForceMode.Impulse);
-            cAnimator.SetTrigger("Jump");
-            Debug.Log("Boing");
+            cAnimator.applyRootMotion = false;
+            cAnimator.SetTrigger(Jump);
+            rBody.AddRelativeForce(j, ForceMode.Impulse);
         }
         else Debug.Log("Not Grounded");
     }
-
-    private void SetJump(bool val)
+    
+    
+    private bool IsGrounded()
     {
-        canJump = val;
-        cAnimator.SetBool("OnGround", val);
+        return Physics.Raycast(transform.position+rayOffset, Vector3.down, .7f);
     }
+
+    public void OnCollisionExit(Collision other)
+    {
+        if (!IsGrounded())
+        {
+            //Set animator bool isFalling to true;
+            Debug.Log("Left the ground");
+            cAnimator.SetBool(OnGround, false);
+        }
+        else Debug.Log("Still on ground");
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (IsGrounded())
+        {
+            Debug.Log("Back on ground");
+            cAnimator.applyRootMotion = true;
+            cAnimator.SetBool(OnGround, true);
+            //Set animator bool is falling to false;
+        }
+        else Debug.Log("Hit something midair");
+    }
+    
+    
+    
     private void OnEnable()
     {
         ctrls.Player.Enable();
@@ -104,9 +134,5 @@ public class CharacterInputRootMotion : MonoBehaviour
     private void OnDisable()
     {
         ctrls.Player.Disable();
-    }
-    private bool IsGrounded()
-    {
-        return Physics.Raycast(transform.position, Vector3.down, .1f);
     }
 }
