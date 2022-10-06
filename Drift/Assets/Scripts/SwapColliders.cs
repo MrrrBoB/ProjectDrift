30f91ab10990a0d1e8239639d53f357b51d888ca
NateMobile <nathoward22@gmail.com>
2022-10-06 15:28:48 -0600

    Made a collider swap script, didn't get to implement yet

---
+++ Drift/Assets/Scripts/SwapColliders.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class SwapColliders : MonoBehaviour
+{
+    public Collider col1;
+
+    public Collider col2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        col1.enabled = true;
+        col2.enabled = false;
+    }
+
+    public void SwapCols()
+    {
+        col1.enabled = !col1.enabled;
+        col2.enabled = !col2.enabled;
+    }
+}
