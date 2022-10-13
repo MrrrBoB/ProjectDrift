f843fe902793b2e6441875e1ddc5f8f0e4e0dc16
Nate <Nathoward22@gmail.com>
2022-10-12 20:59:31 -0600

    Implemented super basic procedural level builder

---
+++ Drift/Assets/Scripts/LevelPiece.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LevelPiece : MonoBehaviour
+{
+    public float levelRotationChange;
+    public Vector3 nextLevelCoordinate;
+    public GameObject nextPiecePlacer;
+    public LevelBuilderData builderData;
+    private int lifeCycles = 5;
+    public UnityEvent deathEvent;
+
+    private void Start()
+    {
+        builderData.UpdateNextCoordinates(nextPiecePlacer.transform.position);
+        builderData.UpdateRotation(levelRotationChange);
+    }
+
+    public void PassCycle()
+    {
+        lifeCycles -= 1;
+        if (lifeCycles <= 0)
+        {
+            deathEvent.Invoke();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Drift/Assets/Scripts/ScriptableObjects/LevelBuilderData.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class LevelBuilderData : ScriptableObject
+{
+   public Vector3 buildCoordinates;
+   public float currentRotationDegree;
+   public Vector3 currentRotationVector;
+   private float rotationSum;
+   public GameObject[] levelPieces;
+   [SerializeField]
+   private GameObject nextPieceToBuild;
+
+   public void UpdateRotation(float newRotation)
+   {
+      rotationSum = currentRotationDegree + newRotation;
+      currentRotationDegree += newRotation;
+      currentRotationVector = new Vector3(0,currentRotationDegree,0 );
+   }
+
+   
+   public float GetCurrentLevelRotation()
+   {
+      return currentRotationDegree;
+   }
+
+   
+   public void UpdateNextCoordinates(Vector3 receivingCoord)
+   {
+      buildCoordinates = receivingCoord;
+   }
+
+   
+   public Vector3 GetNextCoordinates()
+   {
+      return buildCoordinates;
+   }
+
+   public void BuildNextPiece()
+   {
+      nextPieceToBuild = levelPieces[Random.Range(0, levelPieces.Length)];
+      Instantiate(nextPieceToBuild, buildCoordinates, Quaternion.Euler(currentRotationVector));
+   }
+
+   public void resetBuilderData()
+   {
+      currentRotationVector = Vector3.zero;
+      currentRotationDegree = 0;
+      buildCoordinates = Vector3.zero;
+   }
+
+}
