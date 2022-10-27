0c60fd85109eb50a30a284887c5722db744c5121
Nate <Nathoward22@gmail.com>
2022-10-26 21:50:44 -0600

    added a death floor, needs to move with player

--- Drift/Assets/Scripts/CompareTagCollision.cs
+++ Drift/Assets/Scripts/CompareTagCollision.cs
@@ -3,15 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-
+[RequireComponent(typeof(Collider))]
 public class CompareTagCollision : MonoBehaviour
 {
     [SerializeField] private string tagToCompare;
     public UnityEvent collisionEvent;
 
     private void OnCollisionEnter(Collision collision)
-    {
+    { 
         if(collision.gameObject.CompareTag(tagToCompare))
+            collisionEvent.Invoke();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(tagToCompare))
             collisionEvent.Invoke();
     }
 }
