d00c7b82b83d02042306dc00a0951d8098810832
Nate <Nathoward22@gmail.com>
2022-11-16 18:19:03 -0700

    --

--- Drift/Assets/Scripts/SwapColliders.cs
+++ Drift/Assets/Scripts/SwapColliders.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using JetBrains.Annotations;
+
 using UnityEngine;
-using UnityEngine.Events;
 
 public class SwapColliders : MonoBehaviour
 {
