9870395acc5f031369f9095bd4d70cdd7937c1a7
Nate <Nathoward22@gmail.com>
2022-11-18 14:42:46 -0700

    Bug Patches

--- Drift/Assets/Scripts/GenericToolKit/DontDestroyOnLoad.cs
+++ Drift/Assets/Scripts/GenericToolKit/DontDestroyOnLoad.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 
 public class DontDestroyOnLoad : MonoBehaviour
 {
+    private static DontDestroyOnLoad DDLinstance;
     private void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
+            
+        if (DDLinstance == null)
+            DDLinstance = this;
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
